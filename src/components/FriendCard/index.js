55bf6120dcca20fbf21522e0548b630dfdce40d3
Dave Hawkins <dhawkins01@gmail.com>
2019-07-29 19:27:04 -0400

    made changes to cards

--- src/components/FriendCard/index.js
+++ src/components/FriendCard/index.js
@@ -4,25 +4,11 @@ import "./style.css";
 function FriendCard(props) {
   return (
     <div className="card">
-      <div className="img-container">
+      <div className="img-container" onClick={() => props.removeFriend(props.id)}>
         <img alt={props.name} src={props.image} />
       </div>
-      <div className="content">
-        <ul>
-          <li>
-            <strong>Name:</strong> {props.name}
-          </li>
-          <li>
-            <strong>Occupation:</strong> {props.occupation}
-          </li>
-          <li>
-            <strong>Location:</strong> {props.location}
-          </li>
-        </ul>
-      </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
+      
+      
     </div>
   );
 }
