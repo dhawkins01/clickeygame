c7c03e21f984433a830bea0f383cebd2b171cf5d
Dave Hawkins <dhawkins01@gmail.com>
2019-07-30 20:52:28 -0400

    Updates

---
+++ precache-manifest.af2998c020680fc3c0a52a16068c582b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd3adcbab5b0e115a9a39876a219bac1",
+    "url": "/clickeygame/index.html"
+  },
+  {
+    "revision": "8e89d71442c1dff54f89",
+    "url": "/clickeygame/static/css/main.56ef30ef.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clickeygame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "8e89d71442c1dff54f89",
+    "url": "/clickeygame/static/js/main.5a335a57.chunk.js"
+  },
+  {
+    "revision": "814e75e28b0e1996f4b9",
+    "url": "/clickeygame/static/js/runtime~main.7247cb83.js"
+  }
+]);
